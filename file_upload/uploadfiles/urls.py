0cb1b9360097d208298e3b6dedcc51a8332dbfe5
abhi <rathodabhi0@gmail.com>
2020-04-24 16:49:39 -0400

    First graph render

--- file_upload/uploadfiles/urls.py
+++ file_upload/uploadfiles/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='home'),
-    path('data/', views.get_data, name='api-data'),
+    #path('data/', views.get_data, name='api-data'),
+    path('api/data/', views.apidata, name='apidata'),
     path('upload/', views.upload, name='upload'),
     path('form/', views.model_form_upload, name='model_form_upload')
 
--- file_upload/uploadfiles/views.py
+++ file_upload/uploadfiles/views.py
@@ -10,22 +10,20 @@ import json
 
 def base(request):
     return render(request, 'base.html')
-
 def upload(request):
+    return render(request,'upload.html')
+
+def apidata(request):
     context = {}
     df = pd.read_csv('election/LS_20.csv')
     df2 = df[['PARTY', 'TOTAL VOTES']].groupby(['PARTY'], as_index=False).sum()
     df3 = df[['TOTAL VOTES'][0]].sum()
-    df2['Vote share'] = ((df2['TOTAL VOTES'] / df3) * 100)
-    df4 = df2.sort_values(by=['Vote share'], ascending=False)
-    print(df4)
-    df19 = df4[['PARTY', 'Vote share']]
-    json_dict = json.loads(df19.to_json(orient='split'))
-    del json_dict['index']
-    data1 = json_dict
-    #print(data1)
+    df2['Vote_share'] = ((df2['TOTAL VOTES'] / df3) * 100)
+    df4 = df2.sort_values(by=['Vote_share'], ascending=False)
+    df19 = df4[['PARTY', 'Vote_share']]
+    data1 = pd.Series(df19.Vote_share.values, index=df19.PARTY).to_dict()
     context['data'] = data1
-    return render(request, 'upload.html', context)
+    return JsonResponse(data1)
     # elif request.method == 'GET':
     #     data_try = request.GET.get('document')
     #     print(data_try)
